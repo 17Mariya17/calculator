5230881eacff457b53ff101feedb7edd223a69a4
17Mariya17 <146371186+17Mariya17@users.noreply.github.com>
2024-01-22 15:16:51 +0000

    final

--- pages/form.js
+++ pages/form.js
@@ -1,9 +1,7 @@
-// Use insert() function to insert the number in textview.
 function insert(num) {
   document.calc.input_form.value = document.calc.input_form.value + num;
 }
 
-// Use equal() function to return the result based on passed values.
 function equal() {
   var exp = document.calc.input_form.value;
   if (exp) {
@@ -11,11 +9,7 @@ function equal() {
   }
 }
 
-/* Here, we create a backspace() function to remove the number at the end of the numeric series in textview. */
 function backspace() {
   var exp = document.calc.input_form.value;
-  document.calc.input_form.value = exp.substring(
-    0,
-    exp.length - 1
-  ); /* remove the element from total length ? 1 */
+  document.calc.input_form.value = exp.substring(0, exp.length - 1);
 }
